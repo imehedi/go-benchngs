c6d19a3732312e01e352eb0474cfd2ad7f8f1bbf
imehedi <mm13@tc>
2019-11-27 13:59:50 +0000

    [BIO-1] add benchNGS placeholder

--- main.go
+++ main.go
@@ -1,24 +1,38 @@
 package main
 
+import "fmt"
+
 // Different alignment tool produces alignment results in different format.
 // Our procedure do not discriminate the tools based on the tool’s own claim
 // of accuracy. For example, the E-value reported by the BLAST tool is not
 // carried towards the result of our scoring. We collect an information set, R
-// from the alignment results containing: (i) read id (r(n)), (ii) reference se130 quence identifier (ref),
+// from the alignment results containing: (i) query id (q(n)), (ii) reference sequence identifier (ref),
 // (iii) start position of the read (stp). This information
 // is then compared with their counterparts from the GSA.
 type Results struct {
-	readID            string
+	queryID           string // Simulated short reads, q(n) ⊂ Q of variant bp lengths n ⊂ {50, 100, 150, 400};
 	refSeqID          string
 	readStartPosition int8
+}
+
+type QueryLength struct {
+	qLen int
 }
 
 func main() {
 
 	print(fOneCalculator(positivePredictiveValueCalculator(5, 4), positiveRateCalculator(3, 5)))
+	benchNGS("blast", 444)
 
 }
 
+func benchNGS(toolName string, seqLength int8) (fOneScore float64) {
+	fmt.Printf("%v", toolName)
+	fmt.Printf("%d", seqLength)
+	return float64(seqLength)
+}
+
+// Rijsbergen’s accuracy measurement score named here as fOneScore,
 // We used true positive rate (r) and positive predictive rate (p), to compute F1 score.
 // Sensitivity or true positive rate, alternatively called as Recall.
 func fOneCalculator(positivePredictiveValue, positiveRate float64) (fOneScore float64) {
